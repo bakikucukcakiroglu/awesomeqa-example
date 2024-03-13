c1871251caa1a02e4aded01093042153f955eb99
bakikucukcakiroglu <baki.kucukcakiroglu@gmail.com>
2024-03-13 17:35:59 +0300

    remove unnecessary file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,6 +7,9 @@ const nextConfig = {
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   async redirects() {
     return [
       {
